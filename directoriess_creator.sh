db656ee5b7e6189a131de791bd1302cafbf589f4
woo seok <Huan.sock@gmail.com>
2023-12-31 15:57:05 +0100

    why my readme doc disappeared?

--- directoriess_creator.sh
+++ directoriess_creator.sh
@@ -2,25 +2,25 @@
 mkdir -p Notes
 
 # Create subdirectories for Computer Science, Knowledge, and Systems
-mkdir -p Notes/000\ Computer\ science\,\ knowledge\,\ and\ systems/{001\ Knowledge,002\ The\ book,003\ Systems,004\ Data\ processing\ and\ computer\ science,005\ Computer\ programming\,\ programs\,\ and\ data,006\ Special\ computer\ methods,007\ UNI\ Leipzig\ CS\ bachelor}
+mkdir -p Notes/000_Computer_science\,_knowledge\,_and_systems/{001_Knowledge,002_The_book,003_Systems,004_Data_processing_and_computer_science,005_Computer_programming\,_programs\,_and_data,006_Special_computer_methods,007_UNI_Leipzig_CS_bachelor}
 
 # Create subdirectories for Encyclopedias and Books of Facts
-mkdir -p Notes/030\ Encyclopedias\ and\ books\ of\ facts/{031\ Korean,032\ German,033\ English}
+mkdir -p Notes/030_Encyclopedias_and_books_of_facts/{031_Korean,032_German,033_English}
 
 # Create subdirectories for Economics
-mkdir -p Notes/330\ Economics/332\ Financial\ economics
+mkdir -p Notes/330_Economics/332_Financial_economics
 
 # Create subdirectories for Science
-mkdir -p Notes/500\ Science/{510\ Mathematics,512\ Algebra,515\ Analysis,519\ Probabilities\ and\ applied\ mathematics}
+mkdir -p Notes/500_Science/{510_Mathematics,512_Algebra,515_Analysis,519_Probabilities_and_applied_mathematics}
 
 # Create subdirectories for Physics
-mkdir -p Notes/530\ Physics/537\ Electricity\ and\ electronics
+mkdir -p Notes/530_Physics/537_Electricity_and_electronics
 
 # Create subdirectories for Uni Leipzig informatik 
 
-mkdir -p 007.1\ 1.\ Semester/{007.11\ ADS,007.12\ Analysis,007.13\ DS,007.14\ MuP,007.15\ TI1}
-mkdir -p 007.2\ 2.\ Semester/{007.21\ ADS2,007.22\ Linear,007.23\ Logik,007.24\ MuP2,007.25\ OOP}
-mkdir -p 007.3\ 3.\ Semester/{007.31\ Automata,007.32\ DBS,007.33\ Kommunikation,007.34\ Software\ Technik,007.35\ Wahrscheinlichkeit}
+mkdir -p 007.1_1._Semester/{007.11_ADS,007.12_Analysis,007.13_DS,007.14_MuP,007.15_TI1}
+mkdir -p 007.2_2._Semester/{007.21_ADS2,007.22_Linear,007.23_Logik,007.24_MuP2,007.25_OOP}
+mkdir -p 007.3_3._Semester/{007.31_Automata,007.32_DBS,007.33_Kommunikation,007.34_Software_Technik,007.35_Wahrscheinlichkeit}
 
 if command -v tree >/dev/null 2>&1; then
     tree . > blog_tree.txt
